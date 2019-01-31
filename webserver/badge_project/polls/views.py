bbc1685e50d20760972b5395e2c5082d592f33a9
mknauser <33400600+mknauser@users.noreply.github.com>
2019-01-31 14:17:46 +0100

    Done with tutorial 3.

--- webserver/badge_project/polls/views.py
+++ webserver/badge_project/polls/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 
 
-from .models import Choice
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -11,10 +11,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
