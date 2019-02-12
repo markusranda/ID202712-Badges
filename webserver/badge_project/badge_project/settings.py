d5196126b987e1499077c08c0419135ae193f32e
mknauser <33400600+mknauser@users.noreply.github.com>
2019-02-12 17:53:04 +0100

    Its not possible to sign up a new account, with a very basic UI.
    Just go to 192.168.50.50:8080/accounts/signup

--- webserver/badge_project/badge_project/settings.py
+++ webserver/badge_project/badge_project/settings.py
@@ -36,12 +36,14 @@ DEFAULT_FROM_EMAIL = ['randamarkus.95@gmail.com']
 
 INSTALLED_APPS = [
     'badges.apps.BadgesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 
 ]
 
