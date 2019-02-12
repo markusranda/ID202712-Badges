66d5a4fc0a4bb3146cbd5a65c2cdd9a87320d6c9
mknauser <33400600+mknauser@users.noreply.github.com>
2019-02-12 17:05:23 +0100

    Changed the language to English

--- webserver/badge_project/badge_project/settings.py
+++ webserver/badge_project/badge_project/settings.py
@@ -118,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'nb-no'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Europe/Oslo'
 
