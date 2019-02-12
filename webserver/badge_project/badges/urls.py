0e0acac969ce38bba2e48fffb91872d8803b74e0
mknauser <33400600+mknauser@users.noreply.github.com>
2019-02-12 10:35:43 +0100

    Created a "template" for the team to work on.

--- webserver/badge_project/badges/urls.py
+++ webserver/badge_project/badges/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'badges'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
--- webserver/badge_project/badges/views.py
+++ webserver/badge_project/badges/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Users
 
@@ -7,3 +8,28 @@ def index(request):
     all_users_list = Users.objects.all()
     context = {'all_users_list': all_users_list}
     return render(request, 'badges/index.html', context)
+
+
+class DetailView(generic.DetailView):
+    model = Users
+    template_name = 'badges/detail.html'
+
+    '''
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Users.objects.all()
+    '''
+
+
+class IndexView(generic.ListView):
+    template_name = 'badges/index.html'
+    context_object_name = 'all_users_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Users.objects.all
