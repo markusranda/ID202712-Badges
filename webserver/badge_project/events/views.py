5ad3895676ea77aa3161e462cfd384f06f03e9f6
mknauser <33400600+mknauser@users.noreply.github.com>
2019-02-14 13:12:25 +0100

    Events displays two lists. One list with all events, another with active events.

--- webserver/badge_project/events/views.py
+++ webserver/badge_project/events/views.py
@@ -1,9 +1,29 @@
 # Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from badges.models import Events
+from django.views import generic
+
+
+class EventView(generic.ListView):
+    model = Events
+    template_name = 'events/my_events.html'
+    event_historic_list = Events.objects.all()
+    event_active_list = Events.objects.filter(active=1)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 def events(request):
-    return render(request, 'events/my_events.html')
-
-
+    event_historic_list = Events.objects.all()
+    event_active_list = Events.objects.filter(active=1)
+    context = {
+        'event_historic_list': event_historic_list,
+        'event_active_list': event_active_list,
+    }
+    return render(request, 'events/my_events.html', context)
 
