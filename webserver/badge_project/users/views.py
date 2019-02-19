748f2b88ee28c996f0e166b53d2aafccdc9fef84
mknauser <33400600+mknauser@users.noreply.github.com>
2019-02-19 15:14:28 +0100

    Its now possible to access a personal profile page from pressing the button <your_username> at the rightmost position on the navbar.

--- webserver/badge_project/users/views.py
+++ webserver/badge_project/users/views.py
@@ -1,10 +1,26 @@
 from django.urls import reverse_lazy
 from django.views import generic
+from django.views.generic import ListView
 
 from .forms import CustomUserCreationForm
 
+from badges.models import Badges
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+class ProfilePage(generic.CreateView):
+    model = Badges
+    template_name = 'users/profile_page.html'
+
+    def get_context_data(self, **kwargs):
+        #context = super(ListView, self).get_context_data(**kwargs)
+        #context['event_historic_list'] = Events.objects.filter(created_by=self.request.user)
+        #context['event_active_list'] = Events.objects.all().filter(active=1).filter(created_by=self.request.user)
+        return #context
+
+    def get_queryset(self):
+        return
