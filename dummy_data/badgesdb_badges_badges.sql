7f6aa92aa8d79ca824882c2c47b27cea42c66ed3
mknauser <33400600+mknauser@users.noreply.github.com>
2019-02-21 14:49:08 +0100

    Small correction to dummy data.

--- dummy_data/badgesdb_badges_badges.sql
+++ dummy_data/badgesdb_badges_badges.sql
@@ -1,5 +1,5 @@
-INSERT INTO badgesdb.badges_badges (id, name, description, image) VALUES (1, 'Won five game in a row', 'Won five game in a row', 'badge.png');
-INSERT INTO badgesdb.badges_badges (id, name, description, image) VALUES (2, 'Ate the most chocolate', 'Ate the most chocolate', 'badge.png');
-INSERT INTO badgesdb.badges_badges (id, name, description, image) VALUES (3, 'Caffianated', 'Caffianated', 'badge.png');
-INSERT INTO badgesdb.badges_badges (id, name, description, image) VALUES (4, 'Killermachine', 'Killermachine', 'badge.png');
-INSERT INTO badgesdb.badges_badges (id, name, description, image) VALUES (5, 'Mekal Spekal', 'Mekal', 'badge.png');
+INSERT INTO badgesdb.badges_badges (id, name, description, image) VALUES (1, 'Won five game in a row', 'Won five game in a row', '/badges/badge.png');
+INSERT INTO badgesdb.badges_badges (id, name, description, image) VALUES (2, 'Ate the most chocolate', 'Ate the most chocolate', '/badges/badge.png');
+INSERT INTO badgesdb.badges_badges (id, name, description, image) VALUES (3, 'Caffianated', 'Caffianated', '/badges/badge.png');
+INSERT INTO badgesdb.badges_badges (id, name, description, image) VALUES (4, 'Killermachine', 'Killermachine', '/badges/badge.png');
+INSERT INTO badgesdb.badges_badges (id, name, description, image) VALUES (5, 'Mekal Spekal', 'Mekal', '/badges/badge.png');
