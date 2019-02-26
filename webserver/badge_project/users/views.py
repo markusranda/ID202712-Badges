265ef96be2e1a4f855775469660351650fa23cd2
JuleNissen <Sanata Claus>
2019-02-26 13:49:48 +0100

    Added user statistics, added migration files to gitignore
    Feature +: User can now view his/her own statistics in the profile page!!
    
    Added some migration files to gitignore.

--- webserver/badge_project/users/views.py
+++ webserver/badge_project/users/views.py
@@ -3,10 +3,11 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import ListView
 from .forms import CustomUserCreationForm
-from django.db import connection
+from datetime import datetime
 
 from badges.models import Badges
 from events.models import Events
+from users.models import CustomUser
 
 
 class SignUp(generic.CreateView):
@@ -19,8 +20,14 @@ class ProfilePage(generic.ListView):
     model = Badges
     template_name = 'users/profile_page.html'
 
+    #Logic follows the user signed in!!!
     def get_context_data(self, **kwargs):
         context = super(ListView, self).get_context_data(**kwargs)
         context['badges_list'] = Badges.objects.filter(user=self.request.user)
         context['event_active_list'] = Events.objects.all().filter(active=1).filter(created_by=self.request.user)
+
+        # User stats
+        context['badge_count'] = Badges.objects.filter(user=self.request.user).count()
+        context['event_count'] = Events.objects.all().filter(active=0).count()
+
         return context
