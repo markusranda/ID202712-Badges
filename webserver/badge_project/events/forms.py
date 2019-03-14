126e514ae8adfdf467719c74aecd2cda17f1007e
mknauser <33400600+mknauser@users.noreply.github.com>
2019-03-14 17:25:18 +0100

    End of the day.

--- webserver/badge_project/events/forms.py
+++ webserver/badge_project/events/forms.py
@@ -3,6 +3,22 @@ from crispy_forms.layout import Layout, Column, Submit
 from django.forms import ModelForm, NumberInput, Form
 from django import forms
 
+from events.models import Events
+
 
 class EventPinForm(Form):
     event_field = forms.IntegerField()
+
+    def is_valid(self):
+        valid = super(EventPinForm, self).is_valid()
+        if valid:
+            cd = self.cleaned_data
+            event_pin = cd.pop('event_field')
+            try:
+                Events.objects.filter(pin=event_pin).get()
+                return True
+            except:
+                print("")
+
+        else:
+            return False
