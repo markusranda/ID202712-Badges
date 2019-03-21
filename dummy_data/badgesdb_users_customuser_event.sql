559091efd6d99178ecabbb1faa50b3c60ef6816e
JuleNissen <Sanata Claus>
2019-03-21 12:15:06 +0100

    kraken fucked up

---
+++ dummy_data/badgesdb_users_customuser_event.sql
@@ -0,0 +1,3 @@
+INSERT INTO badgesdb.users_customuser_event (id, customuser_id, events_id) VALUES (1, 1, 2);
+INSERT INTO badgesdb.users_customuser_event (id, customuser_id, events_id) VALUES (2, 1, 4);
+INSERT INTO badgesdb.users_customuser_event (id, customuser_id, events_id) VALUES (3, 1, 6);
