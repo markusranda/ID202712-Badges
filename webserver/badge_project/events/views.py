ad9ea57a09dabe513e1dfddc5534ce9519daf940
Markus Randa <randamarkus.95@gmail.com>
2019-03-21 12:13:05 +0100

    Updated the views showing correct data.

--- webserver/badge_project/events/views.py
+++ webserver/badge_project/events/views.py
@@ -18,8 +18,10 @@ class EventView(LoginRequiredMixin, generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ListView, self).get_context_data(**kwargs)
-        context['event_historic_list'] = Events.objects.filter(created_by=self.request.user)
-        context['event_active_list'] = Events.objects.all().filter(active=1).filter(created_by=self.request.user)
+        current_user = self.request.user
+        context['event_historic_list'] = current_user.event.values_list()
+        context['event_active_list'] = Events.objects.filter(created_by=current_user)
+
         return context
 
 
