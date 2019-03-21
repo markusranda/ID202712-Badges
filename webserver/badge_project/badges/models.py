21228896dc022d9465eb300b6b95d12ed954f52d
Markus Randa <randamarkus.95@gmail.com>
2019-03-21 14:29:19 +0100

    Created a more specific junction table for the eventbadges.

--- webserver/badge_project/badges/models.py
+++ webserver/badge_project/badges/models.py
@@ -5,6 +5,3 @@ class Badges(models.Model):
     name = models.CharField(max_length=25)
     description = models.CharField(max_length=255)
     image = models.ImageField('/badges/', blank=True)
-    user = models.ManyToManyField('users.CustomUser', blank=True)
-    event = models.ManyToManyField('events.Events', blank=True)
-
--- webserver/badge_project/events/models.py
+++ webserver/badge_project/events/models.py
@@ -13,3 +13,8 @@ class BadgeRequests(models.Model):
     user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, related_name='request')
     event = models.ForeignKey('Events', on_delete=models.CASCADE, related_name='request')
     badge = models.ForeignKey('badges.Badges', on_delete=models.CASCADE, related_name='request')
+
+
+class EventBadges(models.Model):
+    event = models.ForeignKey('Events', on_delete=models.CASCADE, related_name='eventbadge')
+    badge = models.ForeignKey('badges.Badges', on_delete=models.CASCADE, related_name='eventbadge')
