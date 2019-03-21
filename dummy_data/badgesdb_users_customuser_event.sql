2a6b09c9e4c1e00b9bc0615c3b81830430dcded1
JuleNissen <Sanata Claus>
2019-03-21 12:15:06 +0100

    Revert "kraken fucked up"
    This reverts commit 559091efd6d99178ecabbb1faa50b3c60ef6816e.

--- dummy_data/badgesdb_users_customuser_event.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO badgesdb.users_customuser_event (id, customuser_id, events_id) VALUES (1, 1, 2);
-INSERT INTO badgesdb.users_customuser_event (id, customuser_id, events_id) VALUES (2, 1, 4);
-INSERT INTO badgesdb.users_customuser_event (id, customuser_id, events_id) VALUES (3, 1, 6);
