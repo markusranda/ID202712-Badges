5676346c5e8b9a55d62868afedf7707cb3e0c9a1
mknauser <33400600+mknauser@users.noreply.github.com>
2019-03-28 10:59:39 +0100

    Test is successful, the event_profile_page is now able to switch between user and moderator content.
    This is still done using test data, further improvements will arrive.

--- webserver/badge_project/badges/static/badges/switchEventProfileContent.js
+++ webserver/badge_project/badges/static/badges/switchEventProfileContent.js
@@ -2,12 +2,14 @@
 
 function switchEventProfileContent() {
     const moderatorContent = document.getElementById("eventProfileModContent");
+    const userContent = document.getElementById("eventProfileUserContent");
 
     if (moderatorContent.style.display === "none") {
         moderatorContent.style.display = "block";
+        userContent.style.display = "none"
 
     } else {
         moderatorContent.style.display = "none";
-
+        userContent.style.display = "block";
     }
 }
