655571bb7a1e0ba8f664de826d651e21db331009
mknauser <33400600+mknauser@users.noreply.github.com>
2019-03-28 10:54:39 +0100

    Created a button on the event_profile_page which hides or shows specific content on the page.
    Uses a javascript which is also included in this commit.

---
+++ webserver/badge_project/badges/static/badges/switchEventProfileContent.js
@@ -0,0 +1,13 @@
+
+
+function switchEventProfileContent() {
+    const moderatorContent = document.getElementById("eventProfileModContent");
+
+    if (moderatorContent.style.display === "none") {
+        moderatorContent.style.display = "block";
+
+    } else {
+        moderatorContent.style.display = "none";
+
+    }
+}
