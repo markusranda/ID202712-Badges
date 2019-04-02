1d80d9a4f3dba1885a140c870725c9ebe3aac0aa
mknauser <33400600+mknauser@users.noreply.github.com>
2019-04-02 12:10:50 +0200

    Images is now a model in the application

--- webserver/badge_project/badges/models.py
+++ webserver/badge_project/badges/models.py
@@ -5,3 +5,8 @@ class Badges(models.Model):
     name = models.CharField(max_length=25)
     description = models.CharField(max_length=255)
     image = models.ImageField('/badges/', blank=True)
+
+
+class Images(models.Model):
+    name = models.CharField(max_length=50)
+    url = models.CharField(max_length=255)
