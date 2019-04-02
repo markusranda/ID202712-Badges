d321c1d277b21e0ac861efeb5cf949a6b12e1263
mknauser <33400600+mknauser@users.noreply.github.com>
2019-04-02 11:40:48 +0200

    Templates are now working properly.

--- webserver/badge_project/badges/forms.py
+++ webserver/badge_project/badges/forms.py
@@ -9,8 +9,8 @@ class CreateBadgeForm(Form):
     name = forms.CharField(max_length=50)
     description = forms.CharField(widget=forms.Textarea(attrs={'cols': 40, 'rows': 5, 'placeholder': 'Describe the badge here...'}))
     image = forms.ChoiceField()
-    Select.template_name = 'templates/badges/select.html'
-    Select.option_template_name = 'templates/badges/select_option.html'
+    Select.template_name = 'badges/widgets/select.html'
+    Select.option_template_name = 'badges/widgets/select_option.html'
 
     def __init__(self, *args, **kwargs):
         super(CreateBadgeForm, self).__init__(*args, **kwargs)
@@ -22,9 +22,8 @@ class CreateBadgeForm(Form):
             for file in f:
                 files.append(os.path.join(r, file))
 
-        self.fields['image'] = forms.MultipleChoiceField(
-            choices=[(o, str(o)) for o in files],
-            widget = CheckboxSelectMultiple()
+        self.fields['image'] = forms.ChoiceField(
+            choices=[(o, str(o)) for o in files]
         )
 
 
