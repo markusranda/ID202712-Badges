071dee7c44ca50a97ca60d32e992f8b7e02912bd
mknauser <33400600+mknauser@users.noreply.github.com>
2019-04-02 13:55:23 +0200

    Create new badge form cleans up the selected image data, and makes image_id available on the cleaned_data

--- webserver/badge_project/badges/forms.py
+++ webserver/badge_project/badges/forms.py
@@ -2,6 +2,7 @@ import os
 
 from django import forms
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.forms import ModelForm, Textarea, Form, CheckboxSelectMultiple, Select, RadioSelect
 
 from badges.models import Images
@@ -10,6 +11,20 @@ from badges.models import Images
 class CreateBadgeForm(Form):
     name = forms.CharField(max_length=50)
     description = forms.CharField(widget=forms.Textarea(attrs={'cols': 40, 'rows': 5, 'placeholder': 'Describe the badge here...'}))
-    image = forms.ModelChoiceField(queryset=Images.objects.values_list(), widget=forms.RadioSelect())
+    image_radio_field = forms.ModelChoiceField(queryset=Images.objects.values_list(), widget=forms.RadioSelect())
     RadioSelect.template_name = 'badges/widgets/select.html'
     RadioSelect.option_template_name = 'badges/widgets/select_option.html'
+
+    def clean(self):
+        cd = super().clean()
+        image_object = cd.get('image_radio_field')
+        image_id = image_object[0]
+        try:
+            Images.objects.filter(id=image_id).get()
+
+        except ObjectDoesNotExist:
+            self.add_error('image_id', 'Image does not exist in database!')
+
+        finally:
+            cd['image_id'] = image_id
+            return cd
