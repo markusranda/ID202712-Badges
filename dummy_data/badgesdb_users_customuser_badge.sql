c2bdfdeba4c5b3e330c325648e48351f8449a211
mknauser <33400600+mknauser@users.noreply.github.com>
2019-04-09 10:26:24 +0200

    Removed obsolete dummy data

--- dummy_data/badgesdb_users_customuser_badge.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO badgesdb.users_customuser_badge (id, customuser_id, badges_id) VALUES (4, 1, 1);
-INSERT INTO badgesdb.users_customuser_badge (id, customuser_id, badges_id) VALUES (1, 1, 2);
-INSERT INTO badgesdb.users_customuser_badge (id, customuser_id, badges_id) VALUES (5, 1, 3);
-INSERT INTO badgesdb.users_customuser_badge (id, customuser_id, badges_id) VALUES (2, 1, 4);
-INSERT INTO badgesdb.users_customuser_badge (id, customuser_id, badges_id) VALUES (3, 1, 6);
--- dummy_data/badgesdb_users_customuser_showcase_badge.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO badgesdb.users_customuser_showcase_badge (id, customuser_id, badges_id) VALUES (2, 1, 2);
-INSERT INTO badgesdb.users_customuser_showcase_badge (id, customuser_id, badges_id) VALUES (3, 1, 4);
-INSERT INTO badgesdb.users_customuser_showcase_badge (id, customuser_id, badges_id) VALUES (4, 1, 6);
