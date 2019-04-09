f957967612a28e514159b7a0e6de18060928d4a3
mknauser <33400600+mknauser@users.noreply.github.com>
2019-04-09 15:28:09 +0200

    added id to eventbadges dummy data

--- dummy_data/badgesdb_events_eventbadges.sql
+++ dummy_data/badgesdb_events_eventbadges.sql
@@ -1,6 +1,6 @@
-INSERT INTO badgesdb.events_eventbadges (badge_id, event_id) VALUES (1, 1);
-INSERT INTO badgesdb.events_eventbadges (badge_id, event_id) VALUES (3, 1);
-INSERT INTO badgesdb.events_eventbadges (badge_id, event_id) VALUES (5, 1);
-INSERT INTO badgesdb.events_eventbadges (badge_id, event_id) VALUES (2, 2);
-INSERT INTO badgesdb.events_eventbadges (badge_id, event_id) VALUES (4, 2);
-INSERT INTO badgesdb.events_eventbadges (badge_id, event_id) VALUES (6, 2);
+INSERT INTO badgesdb.events_eventbadges (id, event_id, badge_id) VALUES (1, 1, 1);
+INSERT INTO badgesdb.events_eventbadges (id, event_id, badge_id) VALUES (2, 1, 3);
+INSERT INTO badgesdb.events_eventbadges (id, event_id, badge_id) VALUES (3, 1, 5);
+INSERT INTO badgesdb.events_eventbadges (id, event_id, badge_id) VALUES (4, 2, 2);
+INSERT INTO badgesdb.events_eventbadges (id, event_id, badge_id) VALUES (5, 2, 4);
+INSERT INTO badgesdb.events_eventbadges (id, event_id, badge_id) VALUES (6, 2, 6);
