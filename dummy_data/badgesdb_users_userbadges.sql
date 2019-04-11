39ea3e2533fcb181a48d639f2160317bb4e59c3a
mknauser <33400600+mknauser@users.noreply.github.com>
2019-04-11 14:38:30 +0200

    Added text to the create a new badge form telling the user what the badges are

--- dummy_data/badgesdb_users_userbadges.sql
+++ dummy_data/badgesdb_users_userbadges.sql
@@ -1,6 +1,6 @@
-INSERT INTO badgesdb.users_userbadges (id, badge_id, event_id, user_id, is_showcase, date_earned) VALUES (1, 1, 1, 1, 0,2019-04-11 11:05:13.269);
-INSERT INTO badgesdb.users_userbadges (id, badge_id, event_id, user_id, is_showcase, date_earned) VALUES (2, 2, 1, 1, 0,2019-04-10 11:05:13.269000);
-INSERT INTO badgesdb.users_userbadges (id, badge_id, event_id, user_id, is_showcase, date_earned) VALUES (3, 3, 1, 1, 0,2019-04-09 11:05:13.269000);
-INSERT INTO badgesdb.users_userbadges (id, badge_id, event_id, user_id, is_showcase, date_earned) VALUES (4, 4, 1, 1, 0,2019-04-09 11:05:11.269000);
-INSERT INTO badgesdb.users_userbadges (id, badge_id, event_id, user_id, is_showcase, date_earned) VALUES (5, 5, 1, 1, 0,2019-04-10 11:05:12.269000);
-INSERT INTO badgesdb.users_userbadges (id, badge_id, event_id, user_id, is_showcase, date_earned) VALUES (6, 6, 1, 1, 0,2019-04-11 11:05:11.269000);
+INSERT INTO badgesdb.users_userbadges (id, event_badge_id, user_id, is_showcase, date_earned) VALUES (1, 1, 1, 0, NOW());
+INSERT INTO badgesdb.users_userbadges (id, event_badge_id, user_id, is_showcase, date_earned) VALUES (2, 2, 1, 0, NOW());
+INSERT INTO badgesdb.users_userbadges (id, event_badge_id, user_id, is_showcase, date_earned) VALUES (3, 3, 1, 0, NOW());
+INSERT INTO badgesdb.users_userbadges (id, event_badge_id, user_id, is_showcase, date_earned) VALUES (4, 4, 1, 0, NOW());
+INSERT INTO badgesdb.users_userbadges (id, event_badge_id, user_id, is_showcase, date_earned) VALUES (5, 5, 1, 0, NOW());
+INSERT INTO badgesdb.users_userbadges (id, event_badge_id, user_id, is_showcase, date_earned) VALUES (6, 6, 1, 0, NOW());
