b5f890ff4fc8a8d49ee6ff021a044af34959cd14
mknauser <33400600+mknauser@users.noreply.github.com>
2019-04-11 13:09:58 +0200

    Higher number of character allowed for a badge
    REMEMBER TO MIGRATE!

--- webserver/badge_project/badges/models.py
+++ webserver/badge_project/badges/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Badges(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=55)
     description = models.CharField(max_length=255)
     image = models.ForeignKey('Images', on_delete=models.CASCADE,)
 
